6424f4e99fcc06a3bf55bf7f4affb8ef04786fcd
Mary Grace Nazareno <mgracenazareno@gmail.com>
2021-06-03 00:53:12 -0400

    Complete Model spec requirements

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :description, presence: true
+  validates :ingredient, presence: true, uniqueness: { scope: :cocktail }
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Ingredient < ApplicationRecord
-  has_many :doses, dependent: :destory
+  has_many :doses
 
   validates :name, presence: true, uniqueness: true
 end

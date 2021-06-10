b5f84f124f6b3a1380029c308c0a9985fa687bea
Mary Grace Nazareno <mgracenazareno@gmail.com>
2021-06-09 22:15:09 -0400

    Fix header and banner

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,17 +1,24 @@
 # frozen_string_literal: true
 
 class CocktailsController < ApplicationController
-
   def index
     @cocktails = Cocktail.all
   end
 
   def new
     @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Find(params[:id])
     if @cocktail.save
       redirect_to cocktails_path
     else
       render :new
     end
+  end
+
+  def show
+    @cocktail = Find(params[:id])
   end
 end

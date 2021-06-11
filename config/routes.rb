163623cac1ed26b73552a88946f0f2c29c701b70
Mary Grace Nazareno <mgracenazareno@gmail.com>
2021-06-10 21:48:49 -0400

    Create home partial for pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'pages#home', as: :root
-
+  get 'pages', to: 'pages#index'
   resources :cocktails, only: %i[index new create show]
 end
